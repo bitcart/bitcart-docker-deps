be62aed78421c4501ba1ff1dbda80e4e30c5cf6a
MrNaif2018 <chuff184@gmail.com>
2020-07-20 23:00:30 +0300

    Fix

--- .circleci/push-image.sh
+++ .circleci/push-image.sh
@@ -2,5 +2,5 @@
 
 echo "Pushing to dockerhub repository $DOCKERHUB_DESTINATION"
 docker login --username=$DOCKER_USER --password=$DOCKER_PASS
-docker build -t $DOCKERHUB_DESTINATION "$NODE_NAME/$NODE_VERSION"
+docker build -t $DOCKERHUB_DESTINATION "$NODE_NAME"
 docker push $DOCKERHUB_DESTINATION
