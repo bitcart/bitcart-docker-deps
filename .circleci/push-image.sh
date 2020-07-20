a0e4f49336214fcf54aaef9ceaaf68e07be8090d
MrNaif2018 <chuff184@gmail.com>
2020-07-20 22:54:19 +0300

    Initial commit

---
+++ .circleci/push-image.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Pushing to dockerhub repository $DOCKERHUB_DESTINATION"
+docker login --username=$DOCKER_USER --password=$DOCKER_PASS
+docker build -t $DOCKERHUB_DESTINATION "$NODE_NAME/$NODE_VERSION"
+docker push $DOCKERHUB_DESTINATION
