1510240cfe82175d1a19843b2d7b03f79eab6763
MrNaif2018 <chuff184@gmail.com>
2026-02-19 05:55:07 +0300

    Switch to github actions

--- .circleci/push-image.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-echo "Pushing to dockerhub repository $DOCKERHUB_DESTINATION"
-docker build --pull -t $DOCKERHUB_DESTINATION -t ghcr.io/$DOCKERHUB_DESTINATION -f "$DOCKERHUB_DOCKEFILE" "$NODE_NAME"
-docker push $DOCKERHUB_DESTINATION && docker push ghcr.io/$DOCKERHUB_DESTINATION
